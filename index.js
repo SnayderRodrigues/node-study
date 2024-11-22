677681ddd54dfdef54b3866eb5df736ec933ba8f
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-11-22 19:28:56 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// require("./modules/http");
+
+const dotenv = require("dotenv");
+
+const connectToDatabase = require("./src/database/connect");
+
+dotenv.config();
+
+connectToDatabase();
+
+require("./modules/express");
---
+++ src/database/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.4ccx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Conexão ao banco de dados realizada com sucesso!");
+  } catch (error) {
+    console.log("Ocorreu um erro ao se conectar com o banco de dados: ", error);
+  }
+};
+
+module.exports = connectToDatabase;
